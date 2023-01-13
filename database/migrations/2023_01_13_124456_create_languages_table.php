abe6bb2e7c66fd1ae0aa01e46911bffc8b0332c6
Theo W <theow@Theos-MacBook-Pro.local>
2023-01-13 16:06:32 +0100

    added seeder for languages

--- database/migrations/2023_01_13_124456_create_languages_table.php
+++ database/migrations/2023_01_13_124456_create_languages_table.php
@@ -15,8 +15,9 @@ return new class extends Migration
     {
         Schema::create('languages', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('code');
+            $table->string('name');
+            $table->timestamps();
         });
     }
 
---
+++ database/seeders/LanguageSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Language;
+
+class LanguageSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Language::factory()->count(247)->create();
+    }
+}
