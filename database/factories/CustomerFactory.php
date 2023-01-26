b74578818d922ce40eb7a928c6c38ec7d034d2db
Theodor Will <theodor.will@gmail.com>
2023-01-26 11:59:00 +0100

    added a customer model

---
+++ database/factories/CustomerFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Customer>
+ */
+class CustomerFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $type = $this->faker->randomElement(['individual', 'business']);
+        $name = $type === 'individual' ? $this->faker->name() : $this->faker->company();
+
+
+        return [
+            'name' => $name,
+            'type' => $type,
+            'email' => $this->faker->unique()->safeEmail(),
+            'address' => $this->faker->streetAddress(),
+            'city' => $this->faker->city(),
+            'state' => $this->faker->state(),
+            'zip' => $this->faker->postcode(),
+        ];
+    }
+}
